3fa25e51d9202520faa8b5f186e436763617245f
Emma Rosas <Emma.Rosas@student.csulb.edu>
2023-12-13 21:37:40 -0800

    Alignment

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,7 +4,7 @@ from .views import ContactListView,ContactCreateView,ContactDetailView,ContactUp
 
 urlpatterns = [
     path("", ContactListView.as_view(), name="contact_list"),
-    path("create/", ContactCreateView.as_view, name="contact_create"),
+    path("create/", ContactCreateView.as_view(), name="contact_create"),
     path("contacts/<int:pk>/", ContactDetailView.as_view(), name="contact_detail"),
     path("contacts/<int:pk>/update", ContactUpdateView.as_view(), name="contact_update"),
     path("contacts/<int:pk>/delete", ContactDeleteView.as_view(), name="contact_delete"),
